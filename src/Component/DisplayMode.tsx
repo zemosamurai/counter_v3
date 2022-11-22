34e974fe35710a23811c7d307820cd9fc7b9ab58
zemosamurai <zemosamurai@gmail.com>
2022-11-22 23:21:27 +0300

    new DisplayMode (setting display)

---
+++ src/Component/DisplayMode.tsx
@@ -0,0 +1,43 @@
+import React, {ChangeEvent} from 'react';
+import s from "./SettingCounter.module.css";
+
+type DisplayPropsType = {
+    minValue: number
+    setMinValue: (value: number) => void
+    maxValue: number
+    setMaxValue: (value: number) => void
+}
+
+export const DisplayMode = ({maxValue, minValue, setMinValue, setMaxValue}: DisplayPropsType) => {
+    const errorMax = maxValue <= minValue
+    const error = errorMax || minValue < 0
+
+    const onChangeMaxHandler = (e: ChangeEvent<HTMLInputElement>) => setMaxValue(+e.currentTarget.value)
+    const onChangeStartHandler = (e: ChangeEvent<HTMLInputElement>) => setMinValue(+e.currentTarget.value)
+
+    return (
+        <div className={s.count}>
+            <div>
+                <p>max value: </p>
+                <input
+                    value={maxValue}
+                    className={errorMax ? s.errorInput : ''}
+                    type={"number"}
+                    onChange={onChangeMaxHandler}
+                />
+            </div>
+            <div className={s.elemWrap}>
+                <p>start value: </p>
+                <input
+                    value={minValue}
+                    type={"number"}
+                    onChange={onChangeStartHandler}
+                    className={error ? s.errorInput : ''}
+                />
+            </div>
+        </div>
+    )
+
+
+
+};
