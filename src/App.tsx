735d46a19b4d4d14f7fe5470d6912365a49c2c5c
zemosamurai <zemosamurai@gmail.com>
2022-11-23 18:06:27 +0300

    refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,36 @@ import './App.css';
 import {Counter} from "./Component/Counter";
 
 function App() {
-    const minValue = 0
-    const maxValue = 5
+    const [minValue, setMinValue] = useState(0)
+    const [maxValue, setMaxValue] = useState(5)
     const [count, setCount] = useState<number>(minValue)
 
+    const updateCounter = () => {
+        setCount(minValue)
+        localStorage.setItem('minValue', JSON.stringify(minValue))
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+    }
     useEffect(() => {
-        let valueAsString = localStorage.getItem('counterValue')
-        if (valueAsString) {
-            let newValue = JSON.parse(valueAsString)
-            setCount(newValue)
+        let newMinValue = localStorage.getItem('minValue')
+        let newMaxValue = localStorage.getItem('maxValue')
+        if (newMinValue) {
+            setMinValue(JSON.parse(newMinValue))
+            setCount(JSON.parse(newMinValue))
         }
-    }, [])
-
-    useEffect(() => {
-        localStorage.setItem('counterValue', JSON.stringify(count))
-    }, [count])
+        if (newMaxValue) setMaxValue(JSON.parse(newMaxValue))
+    },[])
 
     return (
         <div>
-            <Counter count={count} setCount={setCount} minValue={minValue} maxValue={maxValue}/>
+            <Counter
+                count={count}
+                setCount={setCount}
+                minValue={minValue}
+                setMinValue={setMinValue}
+                maxValue={maxValue}
+                setMaxValue={setMaxValue}
+                updateCounter={updateCounter}
+            />
         </div>
     );
 }
